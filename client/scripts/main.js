85fc6fcc17ebe02578528a47389b80aa090af26a
MrDB <bnuik77212@gmail.com>
2022-05-15 12:33:37 +0900

    메인배너 자동스크롤 애니메이션 타이머 구현

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,6 +1,7 @@
 
 function scrollBanner( scrollamount ){
     var banner = querySelectorCompatible('.banneritems');
+    var bannerIndex = querySelectorCompatible('.contentIndexes');
     var target = scrollamount * -1280;
     var animTimer = setInterval(function(){
         banner.style.left = (banner.offsetLeft + (banner.offsetLeft >= (scrollamount * -1280) ? -80 : 80)) + 'px';
@@ -8,6 +9,23 @@ function scrollBanner( scrollamount ){
             clearInterval(animTimer);
         }
     },15);
+    setChangeSubClassName(bannerIndex,'normalIndex');
+    bannerIndex.children[scrollamount].setAttribute('class','activeIndex');
+    /* For IE5-9 */
+    bannerIndex.children[scrollamount].className = 'activeIndex';
+}
+
+function startBannerScrollTimer( timedelay ){
+    setInterval(function(){
+        window.curBannerIdx = window.curBannerIdx + 1;
+        
+        if( window.curBannerIdx > window.imgs.length - 1 ){
+            window.curBannerIdx = 0;
+        }
+
+        console.log(window.curBannerIdx);
+        scrollBanner(window.curBannerIdx);
+    },timedelay * 1000);
 }
 
 function initBannerImgs(){
@@ -18,13 +36,19 @@ function initBannerImgs(){
     for(var i = 0; i < window.imgs.length; i++){
         bannerImg = appendDOM(banner,'img');
         bannerItem = appendDOM(bannerIndex,'div','normalIndex',{'type':'click','callback':function(event){
-            setChangeSubClassName(bannerIndex,'normalIndex');
-
             event.target = event.target ? event.target : event.srcElement;
-            event.target.setAttribute('class','activeIndex');
-            /* For IE5-9 */
-            event.target.className = 'activeIndex';
 
+            /* 사용자가 이미 선택되어 있는 배너를 다시 선택하면 */
+            /* 같은 위치에 배너를 다시 스크롤 시키면서 */
+            /* 배너가 깜빡이는 현상(Flickering)이 발생함 */
+            /* 그래서 조건을 비교하여 동일한 배너를 */
+            /* 다시 선택 하였는지, 확인하여 동일한 배너이면 */
+            /* 콜백함수를 반환해서, 스크롤 처리를 막음 */
+            if(window.curBannerIdx == event.target.idx){
+                return;
+            }
+
+            window.curBannerIdx = event.target.idx;
             scrollBanner(event.target.idx);
         }});
         bannerItem.idx = i;
@@ -42,6 +66,8 @@ function onMenuButton_unhovered(target){
 }
 
 window.onload = function(){
+    /* 현재 메인에 표시되는 배너의 순서를 기억합니다 */
+    window.curBannerIdx = 0;
     /* 여기에 사용할 배너 이미지 경로들을 추가하시면 됩니다. */
     window.imgs = [
         "./imgs/banner1.jpg",
@@ -49,4 +75,6 @@ window.onload = function(){
         "./imgs/banner3.jpg"
     ];
     initBannerImgs();
+    /* 5초 간격으로 배너를 회전(Spin) 시키는 타이머(Timer) 실행 */
+    startBannerScrollTimer(5);
 }
